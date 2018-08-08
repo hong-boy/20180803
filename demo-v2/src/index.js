101aacae298dcfe3d6b6b3536ab034b76add9718
hong-boy <hong-boy@foxmail.com>
2018-08-08 16:45:38 +0800

    init v2

--- demo-v2/src/index.js
+++ demo-v2/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import Root from './Root.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDom.render(
+    <Root/>,
+    document.querySelector('#root')
+);
